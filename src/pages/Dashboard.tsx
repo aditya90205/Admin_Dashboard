77c703fffdcfff3ae2bc04d559417034b75ba02b
aditya90205 <adityasrivastav90205@gmail.com>
2024-03-10 14:58:46 +0530

    Complete dashboard section

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,6 +3,10 @@ import AdminSidebar from "../components/AdminSidebar";
 import { FaRegBell } from "react-icons/fa";
 import userImg from "../assets/userpic.png";
 import { HiTrendingDown, HiTrendingUp } from "react-icons/hi";
+import data from "../assets/data.json";
+import { BarChart, DoughnutChart } from "../components/Charts";
+import { BiMaleFemale } from "react-icons/bi";
+import DashboardTable from "../components/DashboardTable";
 
 const Dashboard = () => {
   return (
@@ -46,6 +50,54 @@ const Dashboard = () => {
             color="rgb(76 0 255)"
           />
         </section>
+
+        <section className="graphContainer">
+          <div className="revenue-chart">
+            <h2>Revenue & Transaction</h2>
+            {/* Graph Here */}
+            <BarChart
+              data_1={[300, 144, 433, 655, 237, 755, 190]}
+              data_2={[200, 444, 343, 556, 778, 555, 990]}
+              title_1="Revenue"
+              title_2="Transaction"
+              bgColor_1="rgb(0, 115,255)"
+              bgColor_2="rgba(53,162,235,0.8)"
+            />
+          </div>
+          <div className="dashboard-categories">
+            <h2>Inventory</h2>
+            <div>
+              {data.categories.map((i) => (
+                <CategoryItem
+                  key={i.heading}
+                  heading={i.heading}
+                  value={i.value}
+                  color={`hsl(${i.value * 4}, ${i.value}%, 50%)`}
+                />
+              ))}
+            </div>
+          </div>
+        </section>
+
+        <section className="transaction-container">
+          <div className="gender-chart">
+            <h2>Gender Ratio</h2>
+            <DoughnutChart
+              labels={["Female", "Male"]}
+              data={[12, 19]}
+              backgroundColor={["hsl(340,82%,56%)", "rgba(53,162,235,0.8)"]}
+              cutout={90}
+            />
+            <p>
+              <BiMaleFemale />
+            </p>
+          </div>
+          
+
+
+          <DashboardTable data={data.transaction}/>
+
+        </section>
       </main>
     </div>
   );
@@ -81,17 +133,44 @@ const WidgetItem = ({
       )}
     </div>
 
-    <div className="widgetCircle" style={{
-      background: `conic-gradient(
-        ${color} ${(Math.abs(percent)/100) *360}deg,
+    <div
+      className="widgetCircle"
+      style={{
+        background: `conic-gradient(
+        ${color} ${(Math.abs(percent) / 100) * 360}deg,
         rgb(255,255,255) 0
-      )`
-    }}>
-      <span style={{
-        color
-      }}>{percent}%</span>
+      )`,
+      }}
+    >
+      <span
+        style={{
+          color,
+        }}
+      >
+        {percent}%
+      </span>
     </div>
   </article>
+);
+
+interface CategoryItemProps {
+  color: string;
+  value: number;
+  heading: string;
+}
+const CategoryItem = ({ color, value, heading }: CategoryItemProps) => (
+  <div className="category-item">
+    <h5>{heading}</h5>
+    <div>
+      <div
+        style={{
+          backgroundColor: color,
+          width: `${value}%`,
+        }}
+      ></div>
+    </div>
+    <span>{value}%</span>
+  </div>
 );
 
 export default Dashboard;
