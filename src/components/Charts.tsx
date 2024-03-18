c611580c48f6b9c3e435dcc78558e3d13c08951d
aditya90205 <adityasrivastav90205@gmail.com>
2024-03-18 14:37:45 +0530

    Added Pie Chart Section

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -10,7 +10,7 @@ import {
   ChartOptions,
   ArcElement,
 } from "chart.js";
-import { Bar, Doughnut } from "react-chartjs-2";
+import { Bar, Doughnut, Pie } from "react-chartjs-2";
 
 ChartJS.register(
   CategoryScale,
@@ -138,4 +138,43 @@ export const DoughnutChart = ({
   };
 
   return <Doughnut data={doughnutData} options={doughnutOptions} />;
+};
+
+
+
+interface PieChartProps {
+  labels: string[];
+  data: number[];
+  backgroundColor: string[];
+  offset?: number[];
+}
+
+export const PieChart = ({
+  labels,
+  data,
+  backgroundColor,
+  offset,
+}: PieChartProps) => {
+  const pieChartData: ChartData<"pie", number[], string> = {
+    labels,
+    datasets: [
+      {
+        data,
+        backgroundColor,
+        borderWidth:1,
+        offset,
+      },
+    ],
+  };
+
+  const pieChartOptions: ChartOptions<"pie"> = {
+    responsive: true,
+    plugins: {
+        legend: {
+            display:false,
+        }
+    },
+  };
+
+  return <Pie data={pieChartData} options={pieChartOptions} />;
 };
